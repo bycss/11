651ee980d069f79d395fbd81596a88e201924386
bycss <121210295+bycss@users.noreply.github.com>
2025-06-15 12:00:32 +0900

    自动更新: 手动导出代码到GitHub

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import Calculator from './Calculator';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <Calculator />
-    </div>
+    <Calculator />
   );
 }
 
