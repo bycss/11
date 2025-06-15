c6852f1154823a3156d7fee62a1fc1433cf831dc
bycss <121210295+bycss@users.noreply.github.com>
2025-06-15 12:00:34 +0900

    自动更新: 手动导出代码到GitHub

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Calculator from './Calculator'
+import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <Calculator />
-    </div>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
